0a108b1aacb12a2f421f9a49ce6a8db2446ecb30
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-03-29 01:05:13 +0000

    Handling Errors

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -56,16 +56,27 @@ export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const query= "interstellar";
+  const [error, setError] = useState('');
+  const query= "sdfsf";
 
   useEffect(function () {
     async function fetchMovies() {
-      setIsLoading(true);
-      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`)
+      try {setIsLoading(true);
+      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
+
+      if(!res.ok) throw new Error("Something went wrong with fetching movies")
   
       const data = await res.json();
+      if (data.Response === "False") throw new Error("Movie not found");
+
       setMovies(data.Search);
-      setIsLoading(false);
+      console.log(data);
+      } catch (err) {
+        console.error(err.message);
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchMovies();
   }, []);
@@ -89,8 +100,11 @@ export default function App() {
           </>
         }/> */}
         <Box>
-          {isLoading ? <Loader /> : <MovieList movies ={movies} />}
-          <MovieList movies={movies} />
+          {/* {isLoading ? <Loader /> : <MovieList movies ={movies} />}
+          <MovieList movies={movies} /> */}
+          {isLoading && <Loader />}
+          {isLoading && !error && <MovieList movies ={movies} />}
+          {error && <ErrorMessage message ={error} />}
         </Box>
 
         <Box>
@@ -104,6 +118,12 @@ export default function App() {
 
 function Loader() {
   return <p className="loader">Loading...</p>;
+}
+
+function ErrorMessage({message}) {
+  return <p className="error">
+    <span>⛔</span> {message}
+  </p>
 }
 
 function NavBar({ children }) {
