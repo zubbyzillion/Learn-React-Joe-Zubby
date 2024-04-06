da604b2b88b647b65630466306e45c482e2ce010
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-04-06 07:23:49 +0100

    Clean up useeffect for movie title

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -342,6 +342,11 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   useEffect(function() {
     if (!title) return;
     document.title = `Movie | ${title}`;
+
+    return function() {
+      document.title = "usePopcorn";
+      console.log(`Clean up effect for movie ${title}`);
+    }
   }, [title]);
 
   return (
