d741296d22cbbfdafd3f49fad0c056814460d6de
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-05-29 12:02:10 +0100

    Initializing State with Callback(localStorage)

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -10,12 +10,17 @@ const average = (arr) =>
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+  
+  // const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(function() {
+    const storedValue = localStorage.getItem("watched")
+    return JSON.parse(storedValue);
+  });
 
-  const tempQuery= "interstellar";
+  // const tempQuery= "interstellar";
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId => (id == selectedId ? null : id)));
@@ -26,13 +31,18 @@ export default function App() {
   }
 
   function handleAddWatched(movie) {
-    setWatched(watched => [...watched, movie]);
+    setWatched((watched) => [...watched, movie]);
+
+    // localStorage.setItem("watched", JSON.stringify([...watched, movie]));
   }
 
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id))
   }
 
+  useEffect(function(){
+    localStorage.setItem("watched", JSON.stringify(watched))
+  }, [watched])
 
   useEffect(function () {
     const controller = new AbortController()
@@ -290,9 +300,10 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     onAddWatched(newWatchedMovie);
     // onCloseMovie();
 
-    setAvgRating(Number(imdbRating));
-    setAvgRating((avgRating) => (avgRating + userRating) / 2);
-  }
+  //   setAvgRating(Number(imdbRating));
+  //   setAvgRating((avgRating) => (avgRating + userRating) / 2);
+  // 
+} // Average Rating
 
   
   useEffect(function () {
@@ -348,7 +359,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
         </div>
         </header>
 
-        <p>{avgRating}</p>
+        {/* <p>{avgRating}</p> */}
 
         <section>
           <div className="rating">
