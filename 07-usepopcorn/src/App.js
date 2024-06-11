694f576484f1a814837350b9b9f39c1b62b0486e
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-06-11 18:11:52 +0100

    How not to select DOM elements

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -158,7 +158,11 @@ function Logo() {
 }
 
 function Search({query, setQuery}) {
-  
+  useEffect(function() {
+    const el = document.querySelector(".search");
+    console.log(el);
+    el.focus();
+  })
   
   return (
     <input
