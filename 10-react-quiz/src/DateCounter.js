df1f44d561372f5331f3844ab1db78a0aceeacb9
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-07-08 03:02:55 +0100

    Working with more code

--- 10-react-quiz/src/DateCounter.js
+++ 10-react-quiz/src/DateCounter.js
@@ -1,18 +1,20 @@
-import { useState, useReducer } from "react";
+import { useReducer } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
-  if(action.type === "inc") return state + 1;
-  if(action.type === "dec") return state - 1;
-  if(action.type === "setCount") return action.payload;
+  // if(action.type === "inc") return state + 1;
+  // if(action.type === "dec") return state - 1;
+  // if(action.type === "setCount") return action.payload;
 }
 
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
-  const [count, dispatch] = useReducer(reducer, 0);
+  // const [step, setStep] = useState(1);
+  const initialState = {count: 0, step: 1};
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
-  const [step, setStep] = useState(1);
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
@@ -37,12 +39,12 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
   };
 
   const reset = function () {
     // setCount(0);
-    setStep(1);
+    // setStep(1);
   };
 
   return (
