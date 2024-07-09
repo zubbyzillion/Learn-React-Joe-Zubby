d45c142ed37d9c7d1612df1d9ee17487f7fb651f
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-07-09 08:50:29 +0100

    Loaded questions from fake API using package json-server

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,9 +1,23 @@
-import DateCounter from "./DateCounter";
+import { useEffect } from "react";
+
+import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.error("Error"));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
