b7d76567fe472061fa861537e8715410255abb69
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-07-09 07:30:56 +0100

    Working with more code

--- 10-react-quiz/src/DateCounter.js
+++ 10-react-quiz/src/DateCounter.js
@@ -1,17 +1,33 @@
 import { useReducer } from "react";
 
+const initialState = { count: 0, step: 1};
+
 function reducer(state, action) {
   console.log(state, action);
-  // if(action.type === "inc") return state + 1;
-  // if(action.type === "dec") return state - 1;
-  // if(action.type === "setCount") return action.payload;
+
+  // return { count: 0, step: 1 };
+
+  switch (action.type) {
+    case "dec":
+      return { ...state, count: state.count - state.step };
+    case "inc":
+        return { ...state, count: state.count + state.step };
+    case "setCount":
+      return { ...state, count: action.payload };
+    case "setStep":
+      return { ...state, step: action.payload };
+    case "reset":
+      return initialState;
+      default:
+        throw new Error("Unknown action");
+  }
 }
 
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
   // const [step, setStep] = useState(1);
-  const initialState = {count: 0, step: 1};
+  const initialState = { count: 0, step: 1 };
   const [state, dispatch] = useReducer(reducer, initialState);
   const { count, step } = state;
 
@@ -39,10 +55,12 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
+    dispatch({type: "setStep", payload: Number(e.target.value)})
     // setStep(Number(e.target.value));
   };
 
   const reset = function () {
+    dispatch({ type: "reset" });
     // setCount(0);
     // setStep(1);
   };
