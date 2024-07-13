20dcf3e5f04165ddb9857936d2cdb5bb477a2451
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-07-13 21:39:58 +0100

    Completed timer functionality

--- 10-react-quiz/src/components/App.js
+++ 10-react-quiz/src/components/App.js
@@ -11,6 +11,8 @@ import FinishScreen from "./FinishScreen";
 import Footer from "./Footer";
 import Timer from "./Timer";
 
+const SECS_PER_QUESTION = 30;
+
 
 const initialState = {
   questions: [],
@@ -21,7 +23,7 @@ const initialState = {
   answer: null,
   points: 0,
   highscore: 0,
-  secondsRemaining: 10,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -39,7 +41,8 @@ function reducer(state, action) {
       };
     case "start":
       return{
-        ...state, status: "active"
+        ...state, status: "active", 
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
       }
     case "newAnswer":
       const question = state.questions.at(state.index);
--- 10-react-quiz/src/components/Timer.js
+++ 10-react-quiz/src/components/Timer.js
@@ -1,15 +1,22 @@
 import { useEffect } from "react";
 
 function Timer({ dispatch, secondsRemaining }) {
+    const mins = Math.floor(secondsRemaining / 60);
+    const seconds = secondsRemaining % 60;
 
     useEffect(function() {
-        setInterval(function() {
+        const id = setInterval(function() {
             dispatch({ type: "tick" })
-        }, 1000)
+        }, 1000);
+
+        return () => clearInterval(id);
     }, [dispatch])
     return (
         <div className="timer">
-            {secondsRemaining}
+            {mins < 10 && "0"}
+            {mins}:
+            {seconds < 10 && "0"}
+            {seconds}
         </div>
     )
 }
