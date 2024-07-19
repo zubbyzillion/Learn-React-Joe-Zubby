26a3b3590ce1b5fbaab1925e9fcc7c0fdccfb3f3
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-07-19 13:14:07 +0100

    Added Components

--- WorldWise/src/pages/Login.jsx
+++ WorldWise/src/pages/Login.jsx
@@ -10,7 +10,7 @@ export default function Login() {
   return (
     <main className={styles.login}>
       <PageNav />
-      
+
       <form className={styles.form}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
