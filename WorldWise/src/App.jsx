ab52cb53f27bdf695034de4db6904248cab66ce8
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-07-19 13:02:25 +0100

    Done building pages

--- WorldWise/src/App.jsx
+++ WorldWise/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
 import Homepage from "./pages/Homepage";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />  } />
+        <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />  } />
         <Route path="*" element={<PageNotFound />}/>
       </Routes>
