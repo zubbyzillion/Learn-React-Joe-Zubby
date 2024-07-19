d9717d70e62e4dca734aa7ee51af6677f6b256ac
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-07-19 12:49:02 +0100

    Building The Pages

--- WorldWise/src/pages/Homepage.jsx
+++ WorldWise/src/pages/Homepage.jsx
@@ -1,17 +1,25 @@
-import { Link } from "react-router-dom"
-import AppNav from "../../components/AppNav"
+import { Link } from "react-router-dom";
+import styles from "./Homepage.module.css";
 import PageNav from "../../components/PageNav"
 
-function Homepage() {
-    return (
-        <div>
-            <PageNav />
-            <AppNav />
-            <h1>WorldWise</h1>
-
-            <Link to="/app">Go to the app</Link>
-        </div>
-    )
+export default function Homepage() {
+  return (
+    <main className={styles.homepage}>
+      <PageNav />
+      
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+        <Link to="/app" className="cta">Start tracking now</Link>
+      </section>
+    </main>
+  );
 }
-
-export default Homepage
