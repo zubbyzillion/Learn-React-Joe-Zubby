f0daf37f4f9f10a9aa75a7c4168a53182ddc7aca
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-07-19 19:52:15 +0100

    Nested Routes and Index Routes

--- WorldWise/components/AppNav.jsx
+++ WorldWise/components/AppNav.jsx
@@ -1,9 +1,17 @@
+import { NavLink } from "react-router-dom";
 import styles from "./AppNav.module.css";
 
 function AppNav() {
     return (
         <nav className={styles.nav}>
-            App Navigation
+            <ul>
+                <li>
+                    <NavLink to="cities">Cities</NavLink>
+                </li>
+                <li>
+                    <NavLink to="countries">Countries</NavLink>
+                </li>
+            </ul>
         </nav>
     )
 }
