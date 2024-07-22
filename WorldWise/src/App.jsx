a517d9a314f899310a3390801e07ce121950fcdc
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-07-22 05:57:49 +0100

    Implemeting The Cities List

--- WorldWise/src/App.jsx
+++ WorldWise/src/App.jsx
@@ -5,8 +5,22 @@ import Login from "./pages/Login";
 import Homepage from "./pages/Homepage";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
+import CityList from "../components/CityList";
+import { useState } from "react";
+
+const BASE_URL ="http://localhost:9000";
 
 function App() {
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] =useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      const res = await fetch(`BASE_URL/cities`);
+      const data
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -15,8 +29,8 @@ function App() {
         <Route path="pricing" element={<Pricing />  } />
         <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />  }>
-        <Route index element={<p>List of Cities</p>} />
-          <Route path="cities"element={<p>List of cities</p>} />
+        <Route index element={<CityList />} />
+          <Route path="cities"element={<CityList />} />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
