7e0434251698cc601f4b41ae7eb5e38429904947
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-07-24 18:28:22 +0100

    Created Custom Provider and Hook

--- Atomic Blog/src/App-v1.js
+++ Atomic Blog/src/App-v1.js
@@ -1,6 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
-import {PostProvider, PostContext } from "./PostContext";
 
 function createRandomPost() {
   return {
@@ -9,11 +8,35 @@ function createRandomPost() {
   };
 }
 
+// 1) CREATE A CONTEXT
+const PostContext = createContext();
 
 function App() {
-  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
   const [isFakeDark, setIsFakeDark] = useState(false);
 
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
     function () {
       document.documentElement.classList.toggle("fake-dark-mode");
@@ -22,21 +45,28 @@ function App() {
   );
 
   return (
+    // 2) PROVIDE VALUE TO COMPONENTS
+    <PostContext.Provider value={{
+      posts: searchedPosts,
+      onClearPost: handleAddPost,
+      onClearPosts: handleClearPosts,
+      searchQuery,
+      setSearchQuery,
+    }}>
     <section>
       <button
         onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
         className="btn-fake-dark-mode"
-        >
+      >
         {isFakeDark ? "☀️" : "🌙"}
       </button>
 
-    <PostProvider>
       <Header/>
       <Main />
       <Archive />
       <Footer />
-    </PostProvider>
     </section>
+    </PostContext.Provider>
   );
 }
 
