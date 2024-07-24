ede4a4f017a3056831a9a2ff52fe5e7748abb454
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-07-24 07:15:38 +0100

    Programmatic Navgation - Navigate

--- WorldWise/src/App.jsx
+++ WorldWise/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -42,7 +42,7 @@ function App() {
         <Route path="pricing" element={<Pricing />  } />
         <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />  }>
-        <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+        <Route index element={<Navigate replace to="cities" />} />
           <Route path="cities"element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path="cities/:id" element={<City />} />
           <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
