a5889867a2209e8648a5e1ea80988856a4c59ced
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-07-24 06:00:04 +0100

    Reading and Setting a Query String

--- WorldWise/components/Map.jsx
+++ WorldWise/components/Map.jsx
@@ -5,13 +5,16 @@ function Map() {
     const [searchParams, setSearchParams] = useSearchParams();
     const lat = searchParams.get("lat");
     const lng = searchParams.get("lng");
-    
+
     return (
         <div className={styles.mapContainer}>
         <h1>Map</h1>
         <h1>
             Position: {lat}, {lng}
         </h1>
+        <button onClick={() => {
+            setSearchParams({lat: 23, lng: 75})
+        }}>Change Position</button>
         </div>
     )
 }
