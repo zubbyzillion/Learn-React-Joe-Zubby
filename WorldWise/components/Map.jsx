3e0826e2c4f4ecd58282fe03e8b4968c155fa605
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-07-25 11:53:06 +0100

    Changed color outlook of the map tile

--- WorldWise/components/Map.jsx
+++ WorldWise/components/Map.jsx
@@ -15,7 +15,7 @@ function Map() {
             <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={false} className={styles.map}>
                 <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
                 />
                 <Marker position={mapPosition}>
                     <Popup>
