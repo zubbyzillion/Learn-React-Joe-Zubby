7ff2cad55ed8f5abf34b29529ee9d1e616010f3b
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-07-25 05:24:40 +0100

    Finishing the CIty View

--- WorldWise/components/City.jsx
+++ WorldWise/components/City.jsx
@@ -1,5 +1,5 @@
-import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
+import { useCities } from "../contexts/CitiesContext";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -11,17 +11,11 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const {getCity, currentCity} useCities();
 
-  // TEMP DATA
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
+  useEffect(function () {
+    getCity(id);
+  }, [id])
 
   const { cityName, emoji, date, notes } = currentCity;
 
