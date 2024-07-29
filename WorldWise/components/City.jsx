85ecd91ff13d1892b1f3416c70e982ec959a40df
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-07-29 11:04:07 +0100

    Rectified Flags not showing on city and country list

--- WorldWise/components/City.jsx
+++ WorldWise/components/City.jsx
@@ -16,6 +16,12 @@ const formatDate = (date) =>
 function City() {
   const { id } = useParams();
   const {getCity, currentCity, isLoading} = useCities();
+  const flagemojiToPNG = (flag) => {
+    var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt()).map(char => String.fromCharCode(char-127397).toLowerCase()).join('')
+    return (
+      <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt='flag' />
+    )
+  }
 
   useEffect(
     function () {
@@ -32,7 +38,7 @@ function City() {
       <div className={styles.row}>
         <h6>City name</h6>
         <h3>
-          <span>{emoji}</span> {cityName}
+          <span>{flagemojiToPNG(emoji)}</span> {cityName}
         </h3>
       </div>
 
