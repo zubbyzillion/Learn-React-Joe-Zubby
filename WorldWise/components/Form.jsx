953ebacc9bf52576c8e5198740ae7fcecb5bf6fc
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-07-29 11:40:06 +0100

    Finished fetching city data into form

--- WorldWise/components/Form.jsx
+++ WorldWise/components/Form.jsx
@@ -4,9 +4,10 @@ import { useEffect, useState } from "react";
 
 import styles from "./Form.module.css";
 import Button from "./Button";
+import Message from "./Message";
+import Spinner from "./Spinner";
 import BackButton from "./BackButton";
 import { useUrlPosition } from "../hooks/useUrlPosition";
-// import { flagemojiToPNG } from "./FlagEmojiToPNG";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -28,6 +29,7 @@ function Form() {
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
   const [emoji, setEmoji] = useState("");
+  const [geocodingError, setGeocodingError] = useState("");
 
   const flagemojiToPNG = (flag) => {
     const countryFlag = flag.toLowerCase()
@@ -42,20 +44,34 @@ function Form() {
     async function fetchCityData() {
       try {
         setIsLoadingGeocoding(true);
+        setGeocodingError("");
+
         const res = await fetch(`${BASE_URL}?latitude=${lat}&longitude=${lng}`);
         const data = await res.json();
         console.log(data);
+
+        if (!data.countryCode) 
+          throw new Error (
+            "That doesn't seem to be a city. Click somewhere else😉"
+        );
+
+
         setCityName(data.city || data.locality || "");
         setCountry(data.countryName);
         setEmoji(flagemojiToPNG(data.countryCode));
         // setEmoji(convertToEmoji(data.countryCode));
       } catch (err) {
+        setGeocodingError(err.message);
       } finally {
         setIsLoadingGeocoding(false);
       }
     }
     fetchCityData();
-  }, [lat, lng])
+  }, [lat, lng]);
+
+  if (isLoadingGeocoding) return <Spinner />
+
+  if (geocodingError) return <Message message={geocodingError} />;
 
   return (
     <form className={styles.form}>
