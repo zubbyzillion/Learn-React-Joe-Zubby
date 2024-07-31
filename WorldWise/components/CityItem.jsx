a14cb34661a688568a9b4625e4b4f998c494a1c9
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-07-31 15:04:45 +0100

    Added Delete City Functionality

--- WorldWise/components/CityItem.jsx
+++ WorldWise/components/CityItem.jsx
@@ -11,8 +11,14 @@ const formatDate = (date) =>
     }).format(new Date(date));
 
 function CityItem({ city }) {
-    const { currentCity } = useCities();
+    const { currentCity, deleteCity } = useCities();
     const { cityName, emoji, date, id, position } = city;
+
+    function handleClick(e) {
+        e.preventDefault();
+        deleteCity(id);
+    }
+
     const flagEmojiToPNG = (flag) => {
         var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt()).map(char => String.fromCharCode(char-127397).toLowerCase()).join('')
         return (
@@ -26,7 +32,7 @@ function CityItem({ city }) {
                 <span className={styles.emoji}>{flagEmojiToPNG(emoji)}</span>
                 <h3 className={styles.name}>{cityName}</h3>
                 <time className={styles.date}>({formatDate(date)})</time>
-                <button className={styles.deleteBtn}>&times;</button>
+                <button className={styles.deleteBtn} onClick={handleClick}>&times;</button>
             </Link>
         </li>
     )
