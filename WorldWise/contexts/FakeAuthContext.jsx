5f0b01ae58ba4cb20ffcb712bb08652bd059b030
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-08-01 08:28:25 +0100

    Finished setting up FakeAuth:Context

--- WorldWise/contexts/FakeAuthContext.jsx
+++ WorldWise/contexts/FakeAuthContext.jsx
@@ -22,14 +22,30 @@ function reducer(state, action) {
     }
 }
 
+const FAKE_USER = {
+    name: "Jack",
+    email: "jack@example.com",
+    password: "qwerty",
+    avatar: "https://i.pravatar.cc/100?u=zz",
+  };
+
 function AuthProvider({ children }) {
     const[user, isAuthenticated, dispatch] = useReducer(reducer, initalState)
 
-    function login(email, password) {}
+    function login(email, password) {
+        if(email === FAKE_USER.email && password === FAKE_USER.password)
+            dispatch({ type: "login", payload: FAKE_USER })
+    }
 
-    function logout() {}
+    function logout() {
+        dispatch({ type: "logout" })
+    }
 
-    return <AuthContext.Provider>{children}</AuthContext.Provider>
+    return (
+        <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    )
 }
 
 function useAuth() {
@@ -37,3 +53,5 @@ function useAuth() {
     if(context === undefined)
         throw new Error("AuthContext was used outside AuthProvider");
 }
+
+export { AuthProvider, useAuth };
