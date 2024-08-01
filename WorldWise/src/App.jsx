a8a8fa0dc7db81ac6c45c55f2d8d41f88b7fa4ba
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-08-01 17:18:24 +0100

    Implemented ProtectedRoutes Functionality and Title&Logo Change

--- WorldWise/src/App.jsx
+++ WorldWise/src/App.jsx
@@ -12,6 +12,7 @@ import City from "../components/City";
 import Form from "../components/Form";
 import { CitiesProvider } from "../contexts/CitiesContext";
 import { AuthProvider } from "../contexts/FakeAuthContext";
+import ProtectedRoutes from "./pages/ProtectedRoutes";
 
 
 
@@ -25,8 +26,10 @@ function App() {
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />  } />
           <Route path="/login" element={<Login />} />
-          <Route path="app" element={<AppLayout />  }>
-          <Route index element={<Navigate replace to="cities" />} />
+          <Route path="app" element={ <ProtectedRoutes>
+            <AppLayout />
+          </ProtectedRoutes> }>
+            <Route index element={<Navigate replace to="cities" />} />
             <Route path="cities"element={<CityList />} />
             <Route path="cities/:id" element={<City />} />
             <Route path="countries" element={<CountryList />} />
