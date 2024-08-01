c9755d0c2bcb88296d6085a902c14f78afd1d211
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-08-01 16:56:15 +0100

    Implemented Logout functionality

--- WorldWise/contexts/FakeAuthContext.jsx
+++ WorldWise/contexts/FakeAuthContext.jsx
@@ -23,7 +23,7 @@ function reducer(state, action) {
 }
 
 const FAKE_USER = {
-    name: "Jack",
+    name: "Zubby",
     email: "jack@example.com",
     password: "qwerty",
     avatar: "https://i.pravatar.cc/100?u=zz",
