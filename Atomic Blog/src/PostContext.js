00d11e3292adbb56f49f39b2742f06676d1c602d
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-08-06 05:08:59 +0100

    Optimizing Context Re-Renders

--- Atomic Blog/src/PostContext.js
+++ Atomic Blog/src/PostContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -36,15 +36,19 @@ function PostProvider({ children }) {
         setPosts([]);
       };
 
-      return (
-        // 2) PROVIDE VALUE TO COMPONENTS
-        <PostContext.Provider value={{
-            posts: searchedPosts,
+      const value = useMemo(()=>{
+        return {
+          posts: searchedPosts,
             onAddPost: handleAddPost,
             onClearPosts: handleClearPosts,
             searchQuery,
             setSearchQuery,
-          }}>
+        };
+      }, [searchedPosts, searchQuery]);
+
+      return (
+        // 2) PROVIDE VALUE TO COMPONENTS
+        <PostContext.Provider value={value}>
             {children}
           </PostContext.Provider>
       );
