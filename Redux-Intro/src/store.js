0d57d3bee862fc22ee2311d25eb8da0490f4c392
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-08-09 05:06:19 +0100

    Adding more states - customer

--- Redux-Intro/src/store.js
+++ Redux-Intro/src/store.js
@@ -1,12 +1,18 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
-const initialState = {
+const initialStateAccount = {
     balance: 0,
     loan: 0,
     loanPurpose: "",
 };
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+    fullName: "",
+    nationalID: "",
+    createdAt: "",
+};
+
+function accountReducer(state = initialStateAccount, action) {
     switch(action.type) {
         case "account/deposit":
             return { ...state, balance: state.balance + action.payload };
@@ -23,7 +29,29 @@ function reducer(state = initialState, action) {
     }
 }
 
-const store = createStore(reducer);
+function customerReducer(state = initialStateCustomer, action) {
+    switch(action.type) {
+        case "customer/createCustomer":
+            return { 
+                ...state, fullName: action.payload.fullName, 
+                nationalID: action.payload.nationalID, 
+                createdAt: action.payload.createdAt, 
+            };
+
+            case "customer/updateName":
+                return { ...state, fullName: action.payload, }
+
+            default: 
+                return state;
+    }
+}
+
+const rootReducer = combineReducers({
+    account: accountReducer,
+    customer: customerReducer,
+});
+
+const store = createStore(rootReducer);
 
 // store.dispatch({ type: "account/deposit", payload: 500 });
 // store.dispatch({ type: "account/withdraw", payload: 200 });
@@ -62,4 +90,16 @@ console.log(store.getState());
 store.dispatch(requestLoan(1000, "Buy a Laptop"));
 console.log(store.getState());
 store.dispatch(payLoan());
+console.log(store.getState());
+
+function createCustomer(fullName, nationalID) {
+    return { type: "customer/createCustomer", payload: { fullName, nationalID, createdAt: new Date().toISOString } };
+} 
+
+function updateName(fullName) {
+    return { type: "account/updateName", payload: fullName }
+}
+
+store.dispatch(createCustomer("Zubby Zillion",3434656));
+console.log(deposit(150));
 console.log(store.getState());
