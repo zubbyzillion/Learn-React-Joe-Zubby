1a5fdb226749db823c88650c31ff5227dd984fb9
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-08-11 18:35:54 +0100

    Revamped Code using redux toolkit - configureStore

--- Redux-Intro/src/features/accounts/AccountOperations.js
+++ Redux-Intro/src/features/accounts/AccountOperations.js
@@ -20,7 +20,8 @@ function AccountOperations() {
   function handleDeposit() {
     if(!depositAmount) return;
 
-    dispatch(deposit(depositAmount, currency));
+    // dispatch(deposit(depositAmount, currency));
+    dispatch(deposit(depositAmount));
     setDepositAmount("");
     setCurrency("USD");
   }
