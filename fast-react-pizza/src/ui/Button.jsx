577dd1d24d2024db44ee8365360a54b5c4244d0b
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-08-23 11:30:13 +0100

    Styled the orderForm and using emoji as a favicon

--- fast-react-pizza/src/ui/Button.jsx
+++ fast-react-pizza/src/ui/Button.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 function Button({ children, disabled, to, type }) {
     
-    const base = "bg-yellow-400 uppercase font-semibold text-stone-900 rounded-full tracking-wide inline-block hover:bg-yellow-300 transition:colors duration-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+    const base = "bg-yellow-400 text-sm uppercase font-semibold text-stone-900 rounded-full tracking-wide inline-block hover:bg-yellow-300 transition:colors duration-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
     const styles = {
         primary: base + " px-4 py-3 md:px-6 md:py-4",
