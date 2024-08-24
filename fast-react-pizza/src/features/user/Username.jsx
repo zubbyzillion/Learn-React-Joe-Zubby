5e881b5735ea4cc9bfb5d6810e66b56cbc2e37f5
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-08-24 05:51:00 +0100

    Modelling user state with redux toolkit

--- fast-react-pizza/src/features/user/Username.jsx
+++ fast-react-pizza/src/features/user/Username.jsx
@@ -1,7 +1,13 @@
+import { useSelector } from "react-redux"
+
 function Username() {
+    const username = useSelector((state) => state.user.username);
+
+    if (!username) return null;
+
     return (
         <div className="text-sm font-semibold hidden md:block">
-            Zubby
+            {username}
         </div>
     )
 }
