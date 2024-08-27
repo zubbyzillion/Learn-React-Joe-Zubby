320bfd5654fbcee1450c473fcf137ea033ed37e1
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-08-27 17:51:46 +0100

    Done building the cart page

--- fast-react-pizza/src/features/cart/Cart.jsx
+++ fast-react-pizza/src/features/cart/Cart.jsx
@@ -2,35 +2,17 @@ import { Link } from 'react-router-dom';
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import EmptyCart from "./EmptyCart";
+import { clearCart, getCart  } from "./cartSlice";
+import { useDispatch, useSelector } from 'react-redux';
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
 
 function Cart() {
   const username = useSelector((state) => state.user.username);
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
+  const dispatch = useDispatch();
+
+  if(!cart.length) return <EmptyCart />
 
   return (
     <div className="px-4 py-3">
@@ -46,7 +28,7 @@ function Cart() {
 
       <div className="mt-6 space-x-2">
         <Button to="/order/new" type="primary">Order pizzas</Button>
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={() => dispatch(clearCart())}>Clear cart</Button>
       </div>
     </div>
   );
--- fast-react-pizza/src/features/cart/CartOverview.jsx
+++ fast-react-pizza/src/features/cart/CartOverview.jsx
@@ -7,7 +7,7 @@ function CartOverview() {
   const totalCartQuantity = useSelector(getTotalCartQuantity);
   const totalCartPrice = useSelector(getTotalCartPrice);
   
-  // if (!totalCartQuantity) return null;
+  if (!totalCartQuantity) return null;
 
   return (
     <div className="flex items-center justify-between
