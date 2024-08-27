90779a710e5c7929181304d8a71b41ce6aa5b2c8
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-08-27 11:58:09 +0100

    done building the cart overview with redux selectors

--- fast-react-pizza/src/features/menu/MenuItem.jsx
+++ fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -26,9 +26,9 @@ function MenuItem({ pizza }) {
         <p className="font-medium">{name}</p>
         <p className="text-sm italic text-stone-500 capitalize">{ingredients.join(', ')}</p>
         <div className="mt-auto flex items-center justify-between">
-          {!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p className="text-sm uppercase font-medium text-stone-500">Sold out</p>}
+          {!soldOut ? (<p className="text-sm">{formatCurrency(unitPrice)}</p>) : (<p className="text-sm uppercase font-medium text-stone-500">Sold out</p>)}
 
-          {!soldOut && <Button type="small" onClick={handleAddToCart}>Add to cart</Button>}
+          {!soldOut && (<Button type="small" onClick={handleAddToCart}>Add to cart</Button>)}
         </div>
       </div>
     </li>
