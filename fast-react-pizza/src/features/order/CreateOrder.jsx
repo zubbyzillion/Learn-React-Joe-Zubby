cb527974b86a7e457d83d1623d46754909124eab
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-09-01 06:38:41 +0100

    Done with Redux Thunks with createAsyncThunk

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -3,10 +3,11 @@ import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
 import EmptyCart from "../cart/EmptyCart";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { clearCart, getCart, getTotalCartPrice } from "../cart/cartSlice";
 import store from "../../store";
 import { formatCurrency } from "../../utils/helpers";
+import { fetchAddress } from "../user/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -21,6 +22,7 @@ function CreateOrder() {
   const isSubmitting = navigation.state === "submitting";
 
   const formErrors = useActionData();
+  const dispatch = useDispatch();
 
   const cart = useSelector(getCart);
   const totalCartPrice= useSelector(getTotalCartPrice);
@@ -33,6 +35,8 @@ function CreateOrder() {
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let&apos;s go!</h2>
+
+      <button onClick={() => dispatch(fetchAddress())}>Get Position</button>
 
       {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST">
--- fast-react-pizza/src/features/user/userSlice.js
+++ fast-react-pizza/src/features/user/userSlice.js
@@ -19,6 +19,7 @@ export const fetchAddress = createAsyncThunk("user/fetchAddress", async function
     const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
   
     // 3) Then we return an object with the data that we are interested in
+    // Payload of the FULFILLED state
     return { position, address };
 });
 
@@ -39,8 +40,19 @@ const userSlice = createSlice({
     },
   },
   extraReducers: (builder) => builder.addCase(
-    fetchAddress.pending, (state, action) => (state.status = "loading")
-  ),
+    fetchAddress.pending, (state, action) => {state.status = "loading";}
+  )
+  .addCase(
+    fetchAddress.fulfilled, (state, action) => {
+      state.position = action.payload.position;
+      state.address = action.payload.address;
+      state.state = "idle";
+    }
+  )
+  .addCase(fetchAddress.rejected, (state, action) => {
+    state.status = "error";
+    state.error = action.error.message;
+  }),
 });
 
 export const { updateName } = userSlice.actions;
